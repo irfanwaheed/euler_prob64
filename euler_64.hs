631964c59d20d4ca6f5b590f637c132502278423
Irfan Waheed <iwaheed@irfans-air.attlocal.net>
2018-09-12 23:38:05 -0500

    second commit

---
+++ euler_64.hs
@@ -0,0 +1,19 @@
+import Math.NumberTheory.Powers.Squares
+import Data.List
+import Data.Maybe
+
+dlist =[ x | x <- [2..100], isSquare(x) == False]
+
+whileLoop:: Integer->Integer->Integer
+whileLoop  d y =
+    if isPossibleSquare2 (1 + d*y*y) == True
+        then if isSquare'(1 + d*y*y) == True
+                then  (1 + d*y*y) else whileLoop  d (y+1) else whileLoop  d (y+1)
+
+process_dlist:: [Integer]->[Integer]
+process_dlist [] = []
+process_dlist(d:ds) = whileLoop d 1: process_dlist(ds)
+
+list_xsqs = process_dlist(dlist)
+main = print $  dlist !! fromJust (elemIndex (maximum list_xsqs) list_xsqs)
+
